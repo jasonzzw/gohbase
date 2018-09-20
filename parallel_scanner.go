e50f12cacdefa38bd8b982ca042dbb3e71550b11
Ho-Pang Hsu <hsuhp@seagroup.com>
2018-09-20 17:12:51 +0800

    Expose the underlying scan errors of parallel_scanner

--- parallel_scanner.go
+++ parallel_scanner.go
@@ -2,6 +2,7 @@ package gohbase
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"sync"
 
@@ -22,42 +23,64 @@ type parallelScanner struct {
 }
 
 func NewParallelScanner(c *client, rpc *hrpc.Scan, parallel int) hrpc.Scanner {
-	regs := []hrpc.RegionInfo{}
-
-	var scanRequest *hrpc.Scan
-	var err error
-	scanRequest, err = hrpc.NewScanStr(context.Background(), "hbase:meta",
-		hrpc.Filters(filter.NewPrefixFilter([]byte(rpc.Table()))))
-	if err != nil {
-		log.Errorf("Error with scanning hbase:meta table, err=%s", err)
-		return nil
-	}
-	//else, we scan the table info
-	scanner := c.Scan(scanRequest)
-	for {
-		//scan one row
-		if scanRsp, err := scanner.Next(); err != nil {
-			break
-		} else {
-			if regionInfo, _, err := region.ParseRegionInfo(scanRsp); err == nil {
-				regs = append(regs, regionInfo)
-			}
-		}
-	}
-	log.Infof("Retrieve %d regions for table=%s", len(regs), rpc.Table())
-
-	return &parallelScanner{
-		regions:     regs,
+	// Initialize the returning scanner.
+	ret := &parallelScanner{
+		regions:     []hrpc.RegionInfo{},
 		rootClient:  c,
 		rootScan:    rpc,
 		resultsCh:   make(chan *hrpc.Result, 10000),
 		parallelism: parallel,
 	}
+
+	// Scan metadata for the targeting table.
+	scanRequest, err := hrpc.NewScanStr(
+		context.Background(),
+		"hbase:meta",
+		hrpc.Filters(filter.NewPrefixFilter([]byte(rpc.Table()))))
+	if err != nil {
+		log.Errorf(
+			"Error with scanning hbase:meta table, err=%s",
+			err.Error())
+		return nil
+	}
+
+	scanner := c.Scan(scanRequest)
+	for {
+		if scanRsp, err := scanner.Next(); err != nil {
+			// Was there an error?
+			if err != io.EOF {
+				ret.resultsCh <- &hrpc.Result{
+					Error: fmt.Errorf(
+						"Failed to fetch hbase region metadata: %s",
+						err.Error())}
+			}
+
+			// Abort immediately
+			break
+
+		} else if regionInfo, _, err := region.ParseRegionInfo(scanRsp); err != nil {
+			// Report the error!
+			ret.resultsCh <- &hrpc.Result{
+				Error: fmt.Errorf(
+					"Failed to parse hbase region metadata: %s",
+					err.Error())}
+
+			// Keep fetching metadata of the rest regions.
+
+		} else {
+			ret.regions = append(
+				ret.regions, regionInfo)
+		}
+	}
+
+	// Return the scanner!!!
+	log.Infof("Retrieve %d regions for table=%s", len(ret.regions), rpc.Table())
+	return ret
 }
 
 func (p *parallelScanner) fetch() {
-	ch := make(chan hrpc.RegionInfo, 500)
 	//put regions into the channel
+	ch := make(chan hrpc.RegionInfo, 500)
 	go func() {
 		for _, region := range p.regions {
 			ch <- region
@@ -70,30 +93,58 @@ func (p *parallelScanner) fetch() {
 	for i := 0; i < p.parallelism; i++ {
 		wg.Add(1)
 		go func() {
+			defer wg.Done()
+
+			// Retrieve a region to work on
 			for region := range ch {
-				//the worker due with this region
-				scanObj, err := hrpc.NewScanRange(p.rootScan.Context(), p.rootScan.Table(), region.StartKey(), region.StopKey(), hrpc.Families(p.rootScan.Families()), hrpc.Filters(p.rootScan.Filter()))
+				scanObj, err := hrpc.NewScanRange(
+					p.rootScan.Context(),
+					p.rootScan.Table(),
+					region.StartKey(),
+					region.StopKey(),
+					hrpc.Families(p.rootScan.Families()),
+					hrpc.Filters(p.rootScan.Filter()))
+
 				if err != nil {
+					p.resultsCh <- &hrpc.Result{
+						Error: fmt.Errorf(
+							"Failed to create ScanRange: %s",
+							err.Error())}
+
 					log.Warnf("Create scan object err=%s", err)
 					continue
 				}
-				cli := NewClient(p.rootClient.zkClient.GetQuorum(), p.rootClient.options...)
+
+				cli := NewClient(
+					p.rootClient.zkClient.GetQuorum(),
+					p.rootClient.options...)
+
 				sc := cli.Scan(scanObj)
 				for {
 					//scan one row
 					if scanRsp, err := sc.Next(); err != nil {
-						//this region finish
+						if err != io.EOF {
+							p.resultsCh <- &hrpc.Result{
+								Error: fmt.Errorf(
+									"Failed to scan: %s",
+									err.Error())}
+						}
+
+						// We've finished dealing with this region (maybe not gracefully)
 						break
+
 					} else {
 						p.resultsCh <- scanRsp
 					}
 				}
+
 				sc.Close()
 				cli.Close()
 			}
-			wg.Done()
 		}()
 	}
+
+	// Tear down
 	wg.Wait()
 	close(p.resultsCh)
 }
@@ -105,7 +156,7 @@ func (p *parallelScanner) Next() (*hrpc.Result, error) {
 
 	//return nil, nil
 	if result, ok := <-p.resultsCh; ok {
-		return result, nil
+		return result, result.Error
 	} else {
 		return nil, io.EOF
 	}
